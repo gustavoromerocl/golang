8f60efdf3312bd17f946d69588320b6ef26785cf
garomerop <gandresrp@gmail.com>
2025-12-20 08:14:24 -0300

    [section-5]: env var

--- section-5/cmd/api/main.go
+++ section-5/cmd/api/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/gustavoromerocl/social/cmd/internal/env"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 
 	app := &application{
