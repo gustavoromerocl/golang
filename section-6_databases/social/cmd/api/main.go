2f7ac63c431f0e6c20e8bbb6d78971656d858173
garomerop <gandresrp@gmail.com>
2026-01-04 23:18:23 -0300

    [section-6]: configuring db connection pool

--- section-6_databases/social/cmd/api/main.go
+++ section-6_databases/social/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/gustavoromerocl/social/cmd/internal/db"
 	"github.com/gustavoromerocl/social/cmd/internal/env"
 	"github.com/gustavoromerocl/social/cmd/internal/store"
 )
@@ -10,13 +11,32 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:pass@localhost:5432/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("database connection pool established")
+	store := store.NewStorage(db)
 
 	app := &application{
-		config: cfg,
 		store:  *store,
+		config: cfg,
 	}
 
 	mux := app.mount()
